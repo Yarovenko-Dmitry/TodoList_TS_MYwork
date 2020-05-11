320a3d98fa7bd71c714ad31e59b9efe914f61bf4
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-11 14:49:16 +0300

    Updates

---
+++ precache-manifest.ebfc45e1611e069636e5a390a0a2f616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ec387a07443b61fa2360c7ea616a64",
+    "url": "/TodoList_TS_MYwork/index.html"
+  },
+  {
+    "revision": "4c29e4f4ff46ca4a74c9",
+    "url": "/TodoList_TS_MYwork/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "60669ee11d2726f9e879",
+    "url": "/TodoList_TS_MYwork/static/js/2.f4be072f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList_TS_MYwork/static/js/2.f4be072f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c29e4f4ff46ca4a74c9",
+    "url": "/TodoList_TS_MYwork/static/js/main.911a0e2e.chunk.js"
+  },
+  {
+    "revision": "49d512e82aaca169027b",
+    "url": "/TodoList_TS_MYwork/static/js/runtime-main.69bcd3d9.js"
+  }
+]);
